31fb3dfa87b70e4b178c5f53c42e130b4314df86
Sean Yesmunt <sean@lbry.io>
2020-12-15 14:14:51 -0500

    odysee
    update meme
    
    Use contextual "Support" string
    
    add bun icon
    
    trigger build
    
    adwords script when not embed
    
    adwords unless embed
    
    livestream initial commit
    
    cleanup
    
    fix bitwave username
    
    update livestream deets
    
    fix redirect and prevent floating player from activating when leaving the livestream page
    
    update email address and fix button spacing
    
    add live view count + continously poll bitwave to check if stream is live
    
    insert script from component rather than in html
    
    add support for countdown timer
    
    Update livestream.js
    
    fix comment
    
    remove custom ads code on odysee for more parity with master branch
    
    fix rebase
    
    update livestream info
    
    fix typo
    
    poll file reactions for 'real-time' updates
    mainly for livestreams, but will be useful everywhere
    
    emoji buttons for livestream
    
    markdown
    
    highlight streamer in comments
    
    update livestream details
    
    fetch bitwave as soon as the app finds a livestream claim
    
    use SITE_NAME for password card title
    
    update creator
    
    quick fix to add spacing on livestream page when theater mode is enabled
    
    bump lbry-redux
    
    update for travcrypto
    
    update livestream.js
    
    update livestream.js

--- ui/component/commentCreate/view.jsx
+++ ui/component/commentCreate/view.jsx
@@ -8,9 +8,18 @@ import { FormField, Form } from 'component/common/form';
 import Button from 'component/button';
 import ChannelSelection from 'component/selectChannel';
 import usePersistedState from 'effects/use-persisted-state';
-import { FF_MAX_CHARS_IN_COMMENT } from 'constants/form-field';
+import { FF_MAX_CHARS_IN_COMMENT, FF_MAX_CHARS_IN_LIVESTREAM_COMMENT } from 'constants/form-field';
 import { useHistory } from 'react-router';
 import type { ElementRef } from 'react';
+import emoji from 'emoji-dictionary';
+
+const LIVESTREAM_EMOJIS = [
+  emoji.getUnicode('rocket'),
+  emoji.getUnicode('jeans'),
+  emoji.getUnicode('fire'),
+  emoji.getUnicode('heart'),
+  emoji.getUnicode('open_mouth'),
+];
 
 type Props = {
   uri: string,
@@ -26,6 +35,10 @@ type Props = {
   isPostingComment: boolean,
   activeChannel: string,
   setCommentChannel: string => void,
+  bottom: boolean,
+  onSubmit: (string, string) => void,
+  livestream: boolean,
+  authenticated: boolean,
 };
 
 export function CommentCreate(props: Props) {
@@ -42,9 +55,16 @@ export function CommentCreate(props: Props) {
     isPostingComment,
     activeChannel,
     setCommentChannel,
+    onSubmit,
+    bottom,
+    livestream,
+    authenticated,
   } = props;
   const buttonref: ElementRef<any> = React.useRef();
-  const { push } = useHistory();
+  const {
+    push,
+    location: { pathname },
+  } = useHistory();
   const { claim_id: claimId } = claim;
   const [commentValue, setCommentValue] = React.useState('');
   const [charCount, setCharCount] = useState(commentValue.length);
@@ -79,7 +99,7 @@ export function CommentCreate(props: Props) {
 
   function altEnterListener(e: SyntheticKeyboardEvent<*>) {
     const KEYCODE_ENTER = 13;
-    if ((e.ctrlKey || e.metaKey) && e.keyCode === KEYCODE_ENTER) {
+    if ((livestream || e.ctrlKey || e.metaKey) && e.keyCode === KEYCODE_ENTER) {
       e.preventDefault();
       buttonref.current.click();
     }
@@ -96,6 +116,10 @@ export function CommentCreate(props: Props) {
   function handleSubmit() {
     if (activeChannel !== CHANNEL_NEW && commentValue.length) {
       createComment(commentValue, claimId, activeChannel, parentId).then(res => {
+        if (onSubmit) {
+          onSubmit(commentValue, activeChannel);
+        }
+
         if (res && res.signature) {
           setCommentValue('');
 
@@ -113,9 +137,16 @@ export function CommentCreate(props: Props) {
 
   useEffect(() => setCharCount(commentValue.length), [commentValue]);
 
-  if (!hasChannels) {
+  if (!authenticated || !hasChannels) {
     return (
-      <div role="button" onClick={() => push(`/$/${PAGES.CHANNEL_NEW}`)}>
+      <div
+        role="button"
+        onClick={() =>
+          authenticated
+            ? push(`/$/${PAGES.CHANNEL_NEW}?redirect=${pathname}`)
+            : push(`/$/${PAGES.AUTH}?redirect=${pathname}`)
+        }
+      >
         <FormField
           type="textarea"
           name={'comment_signup_prompt'}
@@ -123,7 +154,7 @@ export function CommentCreate(props: Props) {
           label={isFetchingChannels ? __('Comment') : undefined}
         />
         <div className="section__actions">
-          <Button disabled button="primary" label={__('Post --[button to submit something]--')} requiresAuth={IS_WEB} />
+          <Button disabled button="primary" label={__('Post --[button to submit something]--')} />
         </div>
       </div>
     );
@@ -135,6 +166,7 @@ export function CommentCreate(props: Props) {
       className={classnames('comment__create', {
         'comment__create--reply': isReply,
         'comment__create--nested-reply': isNested,
+        'comment__create--bottom': bottom,
       })}
     >
       <FormField
@@ -158,9 +190,30 @@ export function CommentCreate(props: Props) {
         charCount={charCount}
         onChange={handleCommentChange}
         autoFocus={isReply}
-        textAreaMaxLength={FF_MAX_CHARS_IN_COMMENT}
+        textAreaMaxLength={livestream ? FF_MAX_CHARS_IN_LIVESTREAM_COMMENT : FF_MAX_CHARS_IN_COMMENT}
       />
-      <div className="section__actions section__actions--no-margin">
+      {livestream && hasChannels && (
+        <div className="livestream__emoji-actions">
+          {LIVESTREAM_EMOJIS.map(emoji => (
+            <Button
+              key={emoji}
+              disabled={isPostingComment}
+              type="button"
+              button="alt"
+              className="button--emoji"
+              label={emoji}
+              onClick={() => {
+                setCommentValue(commentValue ? `${commentValue} ${emoji}` : emoji);
+              }}
+            />
+          ))}
+        </div>
+      )}
+      <div
+        className={classnames('section__actions', {
+          'section__actions--no-margin': !livestream,
+        })}
+      >
         <Button
           ref={buttonref}
           button="primary"
