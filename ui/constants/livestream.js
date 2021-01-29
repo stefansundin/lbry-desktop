31df067613ae1acd2e68ff36de52964eaa6ea91c
Sean Yesmunt <sean@lbry.io>
2021-01-29 08:01:03 -0500

    update livestream details

--- ui/constants/livestream.js
+++ ui/constants/livestream.js
@@ -1,6 +1,6 @@
-export const LIVE_STREAM_CHANNEL = '@BalkanTechCrypto';
-export const LIVE_STREAM_CHANNEL_CLAIM_ID = 'c2c4897511dcabd65928cd1d6813bd3bc28e822d';
-export const BITWAVE_USERNAME = 'balkantechcrypto';
+export const LIVE_STREAM_CHANNEL = '@Inty';
+export const LIVE_STREAM_CHANNEL_CLAIM_ID = 'e0073f9a82093400db0f22ac1718ae91f8def60a';
+export const BITWAVE_USERNAME = 'inty';
 
 // export const LIVE_STREAM_CHANNEL = '@sy-test';
 // export const LIVE_STREAM_CHANNEL_CLAIM_ID = '84b50bafd028692c90a25b83d0929defc73154b2';
